2696fe0316e979e60253e7ba766f1b3b713aa0b0
braacellettt <104008593+GalangPradwita@users.noreply.github.com>
2024-03-24 00:04:34 +0700

    menambahkan class SayaTubeVideo

--- tpmod6_1302223015/tpmod6_1302223015/Program.cs
+++ tpmod6_1302223015/tpmod6_1302223015/Program.cs
@@ -2,9 +2,38 @@
 {
     internal class Program
     {
+        public class SayaTubeVideo
+        {
+            private int id;
+            private String title;
+            private int playCount;
+
+            //Konstruktor
+            public SayaTubeVideo(String videoTitle)
+            {
+                Random idRandom = new Random();// id random
+                id = idRandom.Next(1000,100000);
+
+                title = videoTitle;
+                playCount = 0;
+            }
+
+            public void IncreasePlayCounting(int Count)
+            {
+                playCount += Count;
+            }
+
+            public void PrintVideoDetails()
+            {
+                Console.WriteLine("ID: " + id + ", Judul: " +  title + ", Play Count: " + playCount);
+            }
+        }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+             SayaTubeVideo sayaTubeVideo = new SayaTubeVideo("Tutorial Design By Contract - [Galang]");
+            sayaTubeVideo.PrintVideoDetails();
+
         }
     }
 }
